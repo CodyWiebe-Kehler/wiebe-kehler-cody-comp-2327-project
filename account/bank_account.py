1564c49ebd0b94a0a32d397e56d4cbfe4e34f878
Cody Wiebe-Kehler <cwiebe-kehler@rrc.ca>
2026-09-14 10:09:10 -0500

    feat: implemented BankAccount class in bank_account.py

--- account/bank_account.py
+++ account/bank_account.py
@@ -18,4 +18,89 @@ class BankAccount():
         self.__account_id = account_id
         self.__owner = owner
         self.__status = status
-        self.balance = balance
+        self.__balance = balance
+
+    @property
+    def account_id(self) -> int:
+        """Gets the account id of the account
+        
+        Returns:
+            int: numeric account id for this account
+        """
+
+        return self.__account_id
+
+    @property
+    def owner(self) -> Client:
+        """Gets the Client object representing the owner of this account
+        
+        Returns:
+            Client: client object representing the owner of this account
+        """
+
+        return self.__owner
+
+    @property
+    def status(self):
+        """Gets the AccountStatus enumeration value representing the current status
+        of this account
+                
+        Returns:
+            AccountStatus: AccountStatus enumeration value representing the 
+            status of this account
+        """
+
+        return self.__status
+
+
+    @property
+    def balance(self) -> Decimal:
+        """Gets the balance of the account
+        
+        Returns:
+            Decimal: decimal value of the balance of the account
+        """
+
+        return self.__balance
+
+    def __str__(self):
+        return f"Account Number: {self.account_id} Balance: ${self.__balance}"
+
+    def update_balance(self, amount : Decimal) -> None:
+        """Changes the balance of the account by the amount argument. Can be 
+        incremented or decremented with positive or negative values respectively.
+                
+        Arguments:
+            Amount (Decimal): decimal value to adjust the account balance by.
+        """
+
+        self.__balance = self.__balance + amount
+
+    def deposit(self, amount : Decimal) -> None:
+        """Adds the given amount to the balance of the account, amount must be 
+        positive
+                    
+            Arguments:
+                Amount (Decimal): positive decimal value to add to the account balance.
+        """
+
+        if amount < 0:
+            raise ValueError("amount must be a value greater than or equal to zero")
+
+        self.__balance = self.__balance + amount
+
+    def withdraw(self, amount : Decimal) -> None:
+        """Subtracts the given amount from the balance of the account, amount must be 
+        positive
+                    
+            Arguments:
+                Amount (Decimal): positive decimal value to subtract from the account balance.
+        """
+
+        if amount < 0:
+            raise ValueError("amount must be a value greater than or equal to zero")
+
+        if amount > self.__balance:
+            raise ValueError("amount cannot exceed the account balance")
+
+        self.__balance = self.__balance - amount
